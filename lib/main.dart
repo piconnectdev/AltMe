632c024ac2fcc3cbbf18b1fa719b6f0b85955279
hawkbee1 <googandads@gmail.com>
2023-02-17 16:53:50 +0100

    proper sentry init

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,8 @@ import 'package:altme/app/app.dart';
 import 'package:altme/bootstrap.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
-Future<void> main() async {
-  await SentryFlutter.init(
-    (options) {
-      options.dsn =
-          'https://b1e6ffd0c1224d64bcaaadd46ea4f24e@o586691.ingest.sentry.io/4504605041688576';
-      // Set tracesSampleRate to 1.0 to capture 100% of transactions
-      // for performance monitoring.
-      // We recommend adjusting this value in production.
-      options.tracesSampleRate = 1.0;
-      options.debug = true;
-    },
-    appRunner: () => bootstrap(
-      () => const App(flavorMode: FlavorMode.production),
-    ),
+void main() {
+  bootstrap(
+    () => const App(flavorMode: FlavorMode.production),
   );
 }
